a0b0edca9337727584116c075ff6da421f3793e4
Muntaha Islam <muntaha.islam0019@gmail.com>
2023-07-05 11:06:02 +0600

    Added funtionality to show feedback on the weight

--- app.py
+++ app.py
@@ -10,12 +10,27 @@ def calculate_bmi():
         inches = float(request.form['inches'])
         height = (feet * 12 + inches) * 0.0254  # Convert height to meters
         bmi = calculate_bmi_value(weight, height)
-        return render_template('result.html', bmi=bmi)
+        weight_difference = calculate_weight_difference(weight, height)
+        return render_template('result.html', bmi=bmi, weight_difference=weight_difference)
     return render_template('index.html')
 
 def calculate_bmi_value(weight, height):
     bmi = weight / (height ** 2)
     return round(bmi, 2)
+
+def calculate_weight_difference(weight, height):
+    # Calculate ideal weight range based on a healthy BMI (18.5 - 24.9)
+    ideal_weight_min = 18.5 * (height ** 2)
+    ideal_weight_max = 24.9 * (height ** 2)
+    
+    if weight < ideal_weight_min:
+        weight_difference = ideal_weight_min - weight
+        return f"You need to gain {round(weight_difference, 2)} kg to reach a healthy weight."
+    elif weight > ideal_weight_max:
+        weight_difference = weight - ideal_weight_max
+        return f"You need to lose {round(weight_difference, 2)} kg to reach a healthy weight."
+    else:
+        return "You are already within a healthy weight range."
 
 if __name__ == '__main__':
     app.run()
